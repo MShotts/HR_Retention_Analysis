1afa1c42bc647200df9cc05a8ebad8973dd623e6
Matthew Shotts <44091462+MShotts@users.noreply.github.com>
2026-02-15 11:16:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,11 @@
 
 import streamlit as st
 import pandas as pd
-import altair as alt
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.set_page_config(
-    page_title="Home"  # Browser tab title
-    # page_icon="🏠",
-    # layout="wide"
-)
-
+# Sets the background color
 st.markdown("""
     <style>
     .stApp {
@@ -17,70 +14,80 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Add clickable link in sidebar
-st.sidebar.markdown(
-    """
-    <a href="https://hrretentionmshotts.streamlit.app/" target="_blank">
-        <button style="
-            background-color: transparent;
-            color: white;
-            padding: 0px 0px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 14px;
-            width: 100%;
-        ">
-            🔗 Example: Predicting Attrition with HR Data
-        </button>
-    </a>
-    """,
-    unsafe_allow_html=True
-)
+st.write("The following analysis is of a Kaggle dataset concerning Employee Attrition & Performance.")
+st.write("This dataset can be found at: https://www.kaggle.com/datasets/pavansubhasht/ibm-hr-analytics-attrition-dataset")
 
-# st.badge("Work in progress")
+st.write("First, let's see what types of data is included, whether there are null values, and the uniqueness of the fields.")
 
-st.markdown('<span style="font-size:48px; color:#9EE493">**Matthew Shotts**</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Delivering Insights | Improving Processes | Empowering Staff</span>', unsafe_allow_html=True)
+HR_df = pd.read_csv('data/WA_Fn-UseC_-HR-Employee-Attrition.csv')
 
-st.link_button("Linkedin Profile", 'https://www.linkedin.com/in/mattshotts/')
-
-df_years_exp=pd.DataFrame({
-    "Domain": ["Project Mgmt","People Mgmt","SAS/SQL","DataViz/PBI","Cloud DW","Agile"],
-    "Years_Experience": [16,15,11,5,4,4]
+# Display the format and other characteristics of the data
+info_df = pd.DataFrame({
+    'Column': HR_df.columns,
+    'Type': HR_df.dtypes.values,
+    'Non-Null': HR_df.count().values,
+    'Null': HR_df.isnull().sum().values,
+    'Unique': HR_df.nunique().values
 })
+st.dataframe(info_df, use_container_width=True)
 
-years_exp_chart = alt.Chart(df_years_exp).mark_bar(
-    color='#86BBD8'
-).encode(
-    x=alt.X('Years_Experience',title='Years of Experience',axis=alt.Axis(tickCount=5)),
-    y=alt.Y('Domain',sort='-x',title=None)
-).configure(
-    background='transparent'
-).configure_axis(
-    labelColor='#FFFFFF',
-    titleColor='#FFFFFF'
-).properties(
-    # width='container',
-    width=510,
-    height=250
-)
-st.altair_chart(years_exp_chart, use_container_width=False)
+st.write("Right away, we can see signs that the data quality is good.")
+st.write("There are no nulls in the data.")
+st.write("For EmployeeNumber, the counts in the Non-Null and Unique columns match which indicates that this key is unique and there is no need to create a surrogate.")
 
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Drove enterprise-level analytics strategy and execution across healthcare, retail, and assessment organizations:</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Delivering cost savings (six-figure annual vendor fee reductions), improved decision-making, and scalable data products used by hundreds of users weekly.</span>', unsafe_allow_html=True)
+st.write("The data types are comprised of integers and objects. This is mixed news since we'll likely need to create nominal fields in order to include them in our analysis.")
 
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Built and led high-performing analytics and visualization teams:</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Recruiting and developing talent, defining roles and operating models, and stabilizing organizations through change including re-organizations that reduced team size by 50% yet had no disruption to service or user experience.</span>', unsafe_allow_html=True)
+st.write("")
+st.write("Next up, let's see the statistical properties of the data.")
+st.dataframe(HR_df.describe().T.round(2), use_container_width=True, hide_index=False)
 
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Designed and operationalized modern data platforms and semantic layers:</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Standing up a Power BI ecosystem (Center of Excellence, standards, governance, and support) and contributing to the launch of new healthcare and assessment products.</span>', unsafe_allow_html=True)
+st.write("EmployeeCount and StandardHours have no variability (std=0) and thus no predictive value. We'll remove them from consideration in later analyses.")
+st.write("We can see that there are survey results, possibly from a Likert scale, such as EnvironmentSatisfaction, JobInvolvement, JobSatisfaction, etc based on the min, 25%, 50%, 75%, and max increasing incrementally from 1-5 or 1-4.")
+st.write("The description of the Kaggle dataset explains that ratings go from low to high (i.e. 1=Low vs 4=Very High). There is no reverse scoring but it should be noted that the text descriptions do vary (Very High, Best, Outstanding)")
 
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Partnered cross-functionally with executives, internal stakeholders, and external vendors:</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Resolving complex data and process issues, validating third-party solutions, and representing analytics functions to senior leadership and international clients.</span>', unsafe_allow_html=True)
+st.write("")
+st.write("Next, let's see how correlated these fields are. For example, I'd expect fields like Age and TotalWorkingYears to be highly correlated.")
 
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Delivered sustained efficiency and quality improvements:</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Modernizing systems and statistical models, introducing agile delivery practices, and reducing critical workflows by up to 3-4 weeks while maintaining accuracy, compliance, and stakeholder trust.</span>', unsafe_allow_html=True)
+numeric_cols = HR_df.select_dtypes(include=['number']).columns.tolist()
+# Removing the two fields that have no variability
+# Should revise this to automatically capture any fields with std=0 and place into the array
+fields_to_remove = {"EmployeeCount","StandardHours"}
+filtered_numeric_cols=[x for x in numeric_cols if x not in fields_to_remove]
+if len(filtered_numeric_cols) < 2:
+    st.error("Need at least 2 numeric columns")
+else:
+    # Calculate correlation
+    corr = HR_df[filtered_numeric_cols].corr()
+    # Create heatmap
+    fig, ax = plt.subplots(figsize=(16, 14))
+    sns.heatmap(
+        corr,
+        annot=True,
+        fmt='.2f',
+        cmap='coolwarm',
+        center=0,
+        square=True,
+        linewidths=1,
+        cbar_kws={"shrink": 0.8},
+        ax=ax
+    )
+    fig.patch.set_alpha(0)
+    ax.set_facecolor("none")
+    ax.set_xticklabels(ax.get_xticklabels(), color="white")
+    ax.set_xticklabels(ax.get_xticklabels(), color="white")
+    ax.set_yticklabels(ax.get_yticklabels(), color="white")
+    ax.collections[0].colorbar.remove()
+    # plt.title('Correlation Matrix Heatmap', fontsize=16)
+    plt.tight_layout()
 
-# Use the following in terminal (lower side ribbon) to run the app
-# streamlit run C:\Users\DrShotts\PycharmProjects\Streamlit\streamlit_app.py
+    st.pyplot(fig, use_container_width=False)
+
+
+# st.write("### Columns Detected as Numeric")
+# numeric_cols = HR_df.select_dtypes(include="number").columns.tolist()
+# st.write(numeric_cols)
+
+
+
+
+# streamlit run C:\Users\DrShotts\PycharmProjects\Kaggle_HR_Retention\streamlit_app.py
